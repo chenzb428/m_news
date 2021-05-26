718215c2db61fc17e6dbb7733ff3f2b611da6502
BiZhiChen01 <1973626642@qq.com>
2021-05-26 21:15:37 +0800

    完成新闻详情页收藏功能

--- src/js/detail.js
+++ src/js/detail.js
@@ -3,14 +3,17 @@ import { getUrlQueryValue } from '../libs/utils';
 
 import Header from '../components/Header';
 import NewsFrame from '../components/Iframe';
+import Follow from '../components/Follow';
 
 ;((doc) => {
 
     const oApp = doc.querySelector('#app');
     const currentNews = JSON.parse(localStorage.getItem('currentNews'));
+    const followedList = JSON.parse(localStorage.getItem('followedList') || '[]');
 
     const init = () => {
         render();
+        bindEvent();
     }
 
     function render() {
@@ -21,8 +24,31 @@ import NewsFrame from '../components/Iframe';
             showRightIcon: false
         });
         const newsFrameTpl = NewsFrame.tpl(currentNews.url);
+        const followTpl = createFollowTpl();
 
-        oApp.innerHTML += headerTpl + newsFrameTpl;
+        oApp.innerHTML += headerTpl + newsFrameTpl + followTpl;
+    }
+
+    function bindEvent() {
+        Follow.bindEvent(doFollow);
+    }
+
+    function createFollowTpl() {
+        const isExist = followedList.find(item => item.uniquekey === currentNews.uniquekey);
+
+        return isExist ? Follow.follow() : Follow.unfollow();
+    }
+
+    function doFollow(status) {
+        let followedList = JSON.parse(localStorage.getItem('followedList') || '[]');
+
+        if (status) {
+            followedList.push(currentNews);
+        } else {
+            followedList = followedList.filter(item => item.uniquekey !== currentNews.uniquekey);
+        }
+
+        localStorage.setItem('followedLiat', JSON.stringify(followedList));
     }
 
     init();
